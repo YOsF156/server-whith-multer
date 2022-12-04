56a5f2514e5e0aeead56a16e24559b354b4c706f
YOsF156 <yosistar@gmail.com>
2022-12-04 15:13:08 +0200

    add env

---
+++ DL/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+require("dotenv").config();
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL,
+            { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
+                if (error) throw error
+                console.log(`Connection Success, State`,
+                    mongoose.connection.readyState);
+            })
+    } catch (error) {
+        console.log(`Error Mongoose`, error)
+    }
+}
+
+module.exports = { connect }
